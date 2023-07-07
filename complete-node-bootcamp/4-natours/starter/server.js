f1ce4a333addc9d0d84e2c7add2bbfcbad6a5e46
uchario <uchario@yahoo.com>
2023-07-07 01:45:52 +0100

    Connecting our database with the Express App

--- complete-node-bootcamp/4-natours/starter/server.js
+++ complete-node-bootcamp/4-natours/starter/server.js
@@ -1,5 +1,18 @@
 const dotenv = require('dotenv');
+const mongoose = require('mongoose');
 dotenv.config({ path: './config.env' });
+
+const DB = process.env.DB.replace('<PASSWORD>', process.env.DB_PASSWORD);
+
+mongoose.connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true
+}).then(conn => {
+    console.log(conn.connections);
+    console.log('DB Connection Successful')
+});
 
 const app = require('./app');
 
