45b436af54790d7714417e5c1da3fab928a4438f
uchario <uchario@yahoo.com>
2023-07-18 23:27:19 +0100

    Making the API better: Pagination

--- complete-node-bootcamp/4-natours/starter/controllers/tourController.js
+++ complete-node-bootcamp/4-natours/starter/controllers/tourController.js
@@ -24,11 +24,26 @@ exports.getAllTours = async (req, res) => {
             query = query.sort('-createdAt');
         }
 
+        // 3). Field limiting
         if(req.query.fields) {
             const fields = req.query.fields.split(',').join(' ');
             query = query.select(fields);
         } else {
             query = query.select('-__v');
+        }
+
+        // 4). Pagination
+        const page = +req.query.page || 1;
+        const limit = +req.query.limit || 100;
+        const skip = (page - 1) * limit;
+
+        query = query.skip(skip).limit(limit);
+
+        if(req.query.page) {
+            const numTours = await Tour.countDocuments();
+            if(skip > numTours) {
+                throw new Error('This page does not exist');
+            }
         }
 
         const tours = await query;
