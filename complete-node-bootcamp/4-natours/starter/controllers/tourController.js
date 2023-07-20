080a17c8da920f4d0378b660083d2cb4cfceca32
uchario <uchario@yahoo.com>
2023-07-20 13:58:35 +0100

    Refactoring API features

--- complete-node-bootcamp/4-natours/starter/controllers/tourController.js
+++ complete-node-bootcamp/4-natours/starter/controllers/tourController.js
@@ -1,4 +1,5 @@
 const Tour = require('../models/tourModel');
+const APIFeatures = require('./../utils/apiFeatures');
 
 // const tours = JSON.parse(
 //         fs.readFileSync(
@@ -14,46 +15,12 @@ exports.aliasTopTours = (req, res, next) => {
 
 exports.getAllTours = async (req, res) => {
     try {
-        const queryObj = {...req.query};
-    
-        const excludedFields = ['page', 'sort', 'limit', 'fields'];
-        excludedFields.forEach(el => delete queryObj[el]);
-        
-        let queryStr = JSON.stringify(queryObj);
-        queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, match => `$${match}`);
-        
-        let query = Tour.find(JSON.parse(queryStr));
-
-        if(req.query.sort) {
-            const sortBy = req.query.sort.split(',').join(' ');
-            query = query.sort(sortBy);
-        }else {
-            query = query.sort('-createdAt');
-        }
-
-        // 3). Field limiting
-        if(req.query.fields) {
-            const fields = req.query.fields.split(',').join(' ');
-            query = query.select(fields);
-        } else {
-            query = query.select('-__v');
-        }
-
-        // 4). Pagination
-        const page = +req.query.page || 1;
-        const limit = +req.query.limit || 100;
-        const skip = (page - 1) * limit;
-
-        query = query.skip(skip).limit(limit);
-
-        if(req.query.page) {
-            const numTours = await Tour.countDocuments();
-            if(skip > numTours) {
-                throw new Error('This page does not exist');
-            }
-        }
-
-        const tours = await query;
+        const features = new APIFeatures(Tour.find(), req.query)
+                                    .filter()
+                                    .sort()
+                                    .limit()
+                                    .paginate();
+        const tours = await features.query;
         // const tours = await Tour.find()
         //                         .where('duration')
         //                         .equals(5)
@@ -75,7 +42,6 @@ exports.getAllTours = async (req, res) => {
         });
     }
 }
-    
 
 exports.getTourById = async (req, res) => {
     try {
