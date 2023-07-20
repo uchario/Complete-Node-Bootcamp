14d62937352c44807021a5998f0d3f7c0f44d49d
uchario <uchario@yahoo.com>
2023-07-21 00:40:48 +0100

    Document Middleware

--- complete-node-bootcamp/4-natours/starter/models/tourModel.js
+++ complete-node-bootcamp/4-natours/starter/models/tourModel.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const slugify = require('slugify');
 
 const tourSchema = new mongoose.Schema({
     name: {
@@ -7,6 +8,9 @@ const tourSchema = new mongoose.Schema({
         unique: true,
         trim: true
     }, 
+    slug: {
+        type: String
+    },
     duration: {
         type: Number,
         required: [true, 'Duration is required'],
@@ -66,9 +70,19 @@ const tourSchema = new mongoose.Schema({
 
 tourSchema.virtual('durationWeeks')
             .get(function() {
-                return this.duration/7;
+                return this.duration / 7;
             }
 );
+
+tourSchema.pre('save', function(next) {
+    this.slug = slugify(this.name, {lower: true})
+    next();
+});
+
+tourSchema.post('save', function(doc, next) {
+    console.log(doc)
+    next();
+});
 
 const Tour = mongoose.model('Tour', tourSchema);
 module.exports = Tour;
