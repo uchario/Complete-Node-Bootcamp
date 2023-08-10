4da726c359773b2b669ef66d3312fc5abe92d465
uchario <uchario@yahoo.com>
2023-08-11 00:47:14 +0100

    Add userSchema.methods.correctPassword

--- complete-node-bootcamp/4-natours/starter/models/userModel.js
+++ complete-node-bootcamp/4-natours/starter/models/userModel.js
@@ -17,7 +17,8 @@ const userSchema = new mongoose.Schema({
     password: {
         type: String,
         required: [true, 'password is required'],
-        minlength: 8
+        minlength: 8,
+        select: false
     },
     passwordConfirm: {
         type: String,
@@ -42,6 +43,10 @@ userSchema.pre('save', async function(next) {
     this.passwordConfirm = undefined;
     next();
 });
+
+userSchema.methods.correctPassword = async function(candidatePassword, userPassword) {
+    return await bcrypt.compare(candidatePassword, userPassword);
+}
 
 const User = mongoose.model('User', userSchema);
 module.exports = User;
