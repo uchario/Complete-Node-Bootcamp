3264aece9573e360eaeba1924dd8bd704d4effcd
uchario <uchario@yahoo.com>
2023-08-11 00:46:36 +0100

    Modify getAllUsers

--- complete-node-bootcamp/4-natours/starter/controllers/userController.js
+++ complete-node-bootcamp/4-natours/starter/controllers/userController.js
@@ -1,10 +1,18 @@
-exports.getAllUsers = (req, res) => {
-    res.status(500)
+const User = require('./../models/userModel')
+const catchAsync = require('../utils/catchAsync');
+
+exports.getAllUsers = catchAsync( async (req, res, next) => {
+    const users = await User.find();
+    res.status(200)
         .json({
-            status: 'error',
-            message: 'Route not configured'
+            status: 'success',
+            results: users.length,
+            data: {
+                users
+            }
         })
-}
+});
+
 exports.getUserById = (req, res) => {
     res.status(500)
         .json({
