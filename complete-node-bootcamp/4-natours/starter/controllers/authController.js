3b7e939c74782392b88120a1a289b5ede59c605f
uchario <uchario@yahoo.com>
2023-08-11 00:45:56 +0100

    Add signToken function

--- complete-node-bootcamp/4-natours/starter/controllers/authController.js
+++ complete-node-bootcamp/4-natours/starter/controllers/authController.js
@@ -1,5 +1,14 @@
+const jwt = require('jsonwebtoken');
+
 const User = require('../models/userModel');
 const catchAsync = require('../utils/catchAsync');
+const AppError = require('../utils/appError');
+
+const signToken = (id) => {
+    return jwt.sign({id}, process.env.JWT_SECRET, {
+        expiresIn: process.env.JWT_EXPIRES_IN
+    });
+}
 
 exports.signup = catchAsync( async(req, res, next) => {
     const newUser = await User.create({
@@ -8,12 +17,36 @@ exports.signup = catchAsync( async(req, res, next) => {
         password: req.body.password,
         passwordConfirm: req.body.passwordConfirm
     });
+
+    const token = signToken(newUser._id);
     
     res.status(201)
         .json({
             status: 'success',
+            token,
             data: {
                 user: newUser
             }
         });
+});
+
+exports.login = catchAsync( async (req, res, next) => {
+    const {email, password} = req.body;
+
+    if(!email || !password) {
+        return next(new AppError('Provide a valid email and password', 400));
+    };
+
+    const user = await User.findOne({email}).select('+password');
+
+    if(!user || !(await user.correctPassword(password, user.password))) {
+        return next(new AppError('Incorrect email or password', 401));
+    }
+
+    const token = signToken(user._id);
+    res.status(200)
+        .json({
+            status: 'success',
+            token
+        })
 });
