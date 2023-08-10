c367f26838e93ba7cbebbba760775898d580db0a
uchario <uchario@yahoo.com>
2023-08-10 23:20:42 +0100

    Modify app.js

--- complete-node-bootcamp/4-natours/starter/app.js
+++ complete-node-bootcamp/4-natours/starter/app.js
@@ -1,7 +1,12 @@
 const express = require('express');
 const morgan = require('morgan');
+
 const tourRouter = require('./routes/tourRoutes');
-const userRouter = require('./routes/userRoutes')
+const userRouter = require('./routes/userRoutes');
+
+const AppError = require('./utils/appError');
+const globalErrorHandler = require('./controllers/errorController');
+
 const app = express();
 
 // Middleware for data about routes
@@ -10,11 +15,6 @@ if(process.env.NODE_ENV === 'development') {
 }
 app.use(express.json()); //enables request params to be accessible
 app.use(express.static(`${__dirname}/public`)); //serving status content
-
-app.use((req, res, next) => {
-    console.log('Hello from the middleware🙄');
-    next();
-});
 
 app.use((req, res, next) => {
     req.requestTime = new Date().toISOString();
@@ -30,5 +30,19 @@ app.use((req, res, next) => {
 // 3. Routes
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
+
+app.all('*', (req, res, next) => {
+    // res.status(404).json({
+    //     status: 'fail',
+    //     message: `Can't find ${req.originalUrl} on this server!`
+    // })
+    // const err = new Error('Something went wrong');
+    // err.status = 'fail';
+    // err.statusCode = 404;
+
+    next(new AppError('Something went wrong', 404));
+});
+
+app.use(globalErrorHandler);
 
 module.exports = app;
