5a3cee2000906bfca7f8a6f974c156e14ae781d2
uchario <uchario@yahoo.com>
2023-08-13 01:51:40 +0100

    Password Reset Functionality: Setting New Password

--- complete-node-bootcamp/4-natours/starter/controllers/authController.js
+++ complete-node-bootcamp/4-natours/starter/controllers/authController.js
@@ -1,5 +1,6 @@
 const jwt = require('jsonwebtoken');
 const { promisify } = require('util');
+const crypto = require('crypto');
 
 const User = require('../models/userModel');
 const catchAsync = require('../utils/catchAsync');
@@ -126,5 +127,29 @@ exports.forgotPassword = catchAsync(async (req, res, next) => {
 });
 
 exports.resetPassword = catchAsync(async (req, res ,next) => {
+    const hashedToken = crypto.createHash('sha256')
+                            .update(req.params.token)
+                            .digest('hex');
+    
+    const user = await User.findOne({
+        passwordResetToken: hashedToken,
+        passwordResetTokenExpiresIn: { $gt: Date.now() }
+    });
 
+    if(!user) {
+        return next(new AppError('Token is invalid or has expired', 404));
+    }
+
+    user.password = req.body.password;
+    user.passwordConfirm = req.body.passwordConfirm;
+    user.passwordResetToken = undefined;
+    user.passwordResetTokenExpiresIn = undefined;
+    await user.save();
+
+    const token = signToken(user._id);
+    res.status(200)
+        .json({
+            status: 'success',
+            token
+        })
 });
