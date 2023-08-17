15e21cf89e709b7fded99f7ddd271a20cf9e955f
uchario <uchario@yahoo.com>
2023-08-17 08:32:12 +0100

    Implementing Rate Limiting

--- complete-node-bootcamp/4-natours/starter/app.js
+++ complete-node-bootcamp/4-natours/starter/app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const morgan = require('morgan');
+const rateLimit = require('express-rate-limit');
 
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
@@ -13,6 +14,15 @@ const app = express();
 if(process.env.NODE_ENV === 'development') {
     app.use(morgan('dev'));
 }
+
+const limiter = rateLimit({
+    max: 100,
+    windowMs: 60 * 60 * 1000,
+    message: 'Too many requests from this IP'
+});
+
+app.use('/api', limiter);
+
 app.use(express.json()); //enables request params to be accessible
 app.use(express.static(`${__dirname}/public`)); //serving status content
 
