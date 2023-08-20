00dd52306b90f3ef44360c8ffb6c783971af1a20
uchario <uchario@yahoo.com>
2023-08-20 01:15:15 +0100

    Modelling Tour Guides: Child Referencing

--- complete-node-bootcamp/4-natours/starter/models/tourModel.js
+++ complete-node-bootcamp/4-natours/starter/models/tourModel.js
@@ -104,7 +104,12 @@ const tourSchema = new mongoose.Schema({
             day: Number
         }
     ],
-    guides: Array
+    guides: [
+        {
+            type: mongoose.Schema.ObjectId,
+            ref: 'User'
+        }
+    ]
 },{
     toJSON: {virtuals: true},
     toObject: {virtuals: true}
@@ -121,11 +126,11 @@ tourSchema.pre('save', function(next) {
     next();
 });
 
-tourSchema.pre('save', async function(next) {
-    const guidesPromises = this.guides.map(async id => await User.findById(id));
-    this.guides = await Promise.all(guidesPromises);
-    next();
-})
+// tourSchema.pre('save', async function(next) {
+//     const guidesPromises = this.guides.map(async id => await User.findById(id));
+//     this.guides = await Promise.all(guidesPromises);
+//     next();
+// });
 
 tourSchema.post('save', function(doc, next) {
     // console.log(doc)
