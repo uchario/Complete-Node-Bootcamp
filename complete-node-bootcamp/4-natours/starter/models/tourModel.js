51f5efb74997051541ee52f7facaebab20196b72
uchario <uchario@yahoo.com>
2023-08-20 01:02:40 +0100

    Modelling Tour Guides: Embedding

--- complete-node-bootcamp/4-natours/starter/models/tourModel.js
+++ complete-node-bootcamp/4-natours/starter/models/tourModel.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 const slugify = require('slugify');
 const validator = require('validator');
+const User = require('./userModel');
 
 const tourSchema = new mongoose.Schema({
     name: {
@@ -102,7 +103,8 @@ const tourSchema = new mongoose.Schema({
             description: String,
             day: Number
         }
-    ]
+    ],
+    guides: Array
 },{
     toJSON: {virtuals: true},
     toObject: {virtuals: true}
@@ -118,6 +120,12 @@ tourSchema.pre('save', function(next) {
     this.slug = slugify(this.name, {lower: true})
     next();
 });
+
+tourSchema.pre('save', async function(next) {
+    const guidesPromises = this.guides.map(async id => await User.findById(id));
+    this.guides = await Promise.all(guidesPromises);
+    next();
+})
 
 tourSchema.post('save', function(doc, next) {
     // console.log(doc)
