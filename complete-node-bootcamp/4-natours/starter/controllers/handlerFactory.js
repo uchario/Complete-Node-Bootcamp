b7640de25c0e0c8ff11705a3d08078bd68d19423
uchario <uchario@yahoo.com>
2023-09-27 17:44:39 +0100

    Add 'updateOne' and 'createOne' functions

--- complete-node-bootcamp/4-natours/starter/controllers/handlerFactory.js
+++ complete-node-bootcamp/4-natours/starter/controllers/handlerFactory.js
@@ -13,3 +13,35 @@ exports.deleteOne = (Model) => catchAsync( async (req, res, next) => {
     });
    
 });
+
+exports.updateOne = (Model) => catchAsync ( async (req, res, next) => {
+    const doc = await Model.findByIdAndUpdate(
+        req.params.id, 
+        req.body, 
+        {
+            new: true,
+            runValidators: true
+        }
+    );
+    if(!doc) {
+        return next(new AppError("Doc ID doesn't exist", 404));
+    }
+    res.status(200)
+    .json({
+        status: 'success',
+        data: {
+            data: doc
+        }
+    });
+});
+
+exports.createOne = (Model) => catchAsync( async (req, res, next) => {
+    const doc = await Model.create(req.body);
+        res.status(201)
+                .json({
+                    status: 'success',
+                    data: {
+                        data: doc
+                    }
+                })
+            });
