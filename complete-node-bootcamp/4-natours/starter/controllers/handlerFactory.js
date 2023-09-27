303981cc7d44a64089ec3bb9292efde46761d106
uchario <uchario@yahoo.com>
2023-09-27 13:35:03 +0100

    Add hanlderFactory for delete ops

---
+++ complete-node-bootcamp/4-natours/starter/controllers/handlerFactory.js
@@ -0,0 +1,15 @@
+const catchAsync = require('../utils/catchAsync');
+const AppError = require('../utils/appError');
+
+exports.deleteOne = (Model) => catchAsync( async (req, res, next) => {
+    const doc = await Model.findByIdAndDelete(req.params.id);
+    if(!doc) {
+        return next(new AppError("Doc ID doesn't exist", 404));
+    }
+    res.status(204)
+    .json({
+        status: 'success',
+        data: null
+    });
+   
+});
