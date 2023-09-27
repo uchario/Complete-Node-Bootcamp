853286146f6d45772c585dd4579f3d27b8bd4e52
uchario <uchario@yahoo.com>
2023-09-27 22:38:33 +0100

    Add missing authorization and authentication

--- complete-node-bootcamp/4-natours/starter/routes/reviewRoutes.js
+++ complete-node-bootcamp/4-natours/starter/routes/reviewRoutes.js
@@ -5,18 +5,26 @@ const authController = require('../controllers/authController');
 
 const router = express.Router({mergeParams: true});
 
+router.use(authController.protect);
+
 router.route('/')
     .get(reviewController.getAllReviews)
     .post(
-        authController.protect, 
-        authController.restrictTo('guide'),
+        authController.restrictTo('user'),
         reviewController.setTourUserIds,
         reviewController.createReview
     );
 
-module.exports = router;
-
+    
 router.route('/:id')
-        .get(reviewController.getReview)
-        .patch(reviewController.updateReview)
-        .delete(reviewController.deleteReview);
+    .get(reviewController.getReview)
+    .patch(
+        authController.restrictTo('user', 'admin'),
+        reviewController.updateReview
+    )
+    .delete(
+        authController.restrictTo('user', 'admin'),
+        reviewController.deleteReview
+    );
+    
+module.exports = router;
