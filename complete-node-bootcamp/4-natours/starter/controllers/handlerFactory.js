5a55a078f475884c7c1bba4fa5133c9163d068e2
uchario <uchario@yahoo.com>
2023-09-27 18:47:25 +0100

    Add 'getOne' and 'getAll'

--- complete-node-bootcamp/4-natours/starter/controllers/handlerFactory.js
+++ complete-node-bootcamp/4-natours/starter/controllers/handlerFactory.js
@@ -1,5 +1,6 @@
 const catchAsync = require('../utils/catchAsync');
 const AppError = require('../utils/appError');
+const APIFeatures = require('./../utils/apiFeatures');
 
 exports.deleteOne = (Model) => catchAsync( async (req, res, next) => {
     const doc = await Model.findByIdAndDelete(req.params.id);
@@ -45,3 +46,46 @@ exports.createOne = (Model) => catchAsync( async (req, res, next) => {
                     }
                 })
             });
+
+exports.getOne = (Model, popOptions) => catchAsync ( async (req, res, next) => {
+    let query =  await Model.findById(req.params.id);
+    query = popOptions ? query.populate(popOptions) : query;
+
+    const doc = query;
+
+    if(!doc) {
+        return next(new AppError("Doc ID doesn't exist", 404));
+    }
+    res.status(200)
+    .json({
+        status: 'success',
+        data: {
+            data: doc
+        }
+    });
+});
+
+exports.getAll = (Model) => catchAsync( async (req, res, next) => {
+    let filter = {};
+    filter = req.params.tourId ? {tour: req.params.tourId} : filter;
+
+    const features = new APIFeatures(Model.find(filter), req.query)
+                                .filter()
+                                .sort()
+                                .limit()
+                                .paginate();
+    const doc = await features.query;
+    // const tours = await Tour.find()
+    //                         .where('duration')
+    //                         .equals(5)
+    //                         .where('difficulty')
+    //                         .equals('easy');
+    res.status(200)
+        .json({
+            status: 'success',
+            results: doc.length,
+            data: {
+                data: doc
+            }
+        });
+});
